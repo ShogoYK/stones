6deb4fa81a1bac10d8ee1b2669342c5ee84a6b02
ShogoYK <shogonicolas@gmail.com>
2022-11-15 00:15:17 -0400

    Commit inicial

---
+++ index.js
@@ -0,0 +1,8 @@
+function showTone(){
+    let tone = document.getElementById('toneSelector').value
+    let toneShow = document.getElementById('showTone')
+    // alert(`Seu tom é ${tone}`)
+    toneShow.innerText = `${tone}`
+}
+
+
